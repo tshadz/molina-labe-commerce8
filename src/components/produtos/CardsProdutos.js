034cec51b3c4fcc5ce59c3359e8283b36a1a6980
tshadz <thais.sombra@hotmail.com>
2021-06-09 19:41:34 -0300

    :package: Implementação de filtro por valor máximo e mínimo e filtro por nome

---
+++ src/components/produtos/CardsProdutos.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const CardContainer = styled.div`
+border: 1px solid black;
+display: flex;
+flex-direction: column;
+`;
+
+const CardInfo = styled.div`
+display: flex;
+flex-direction: column;
+padding: 16px;
+
+p {
+  margin: 4px 0;
+}
+`
+
+const AddCarrinhoButton = styled.button`
+align-self: center;
+margin-top: 4px;
+`
+
+export default class CardsProdutos extends React.Component {
+    render() {
+      const produto = this.props.produto
+      return <CardContainer>
+        <img src={produto.imageUrl}/>
+        <CardInfo>
+          <p>{produto.nome}</p>
+          <p>R${produto.preco},00</p>
+          <AddCarrinhoButton onClick={() => this.props.AddProdutoCarrinho(produto.id)}>
+            Adicionar ao carrinho
+          </AddCarrinhoButton>
+        </CardInfo>
+      </CardContainer>
+    }
+  }
+  
